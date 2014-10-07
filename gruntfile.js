120d1fe7e84ebb8fadc0b47f4934cfc8940b0edc
Adam Laycock <adam@arcath.net>
2014-10-07 10:39:24 +0100

    Move some files around during build

--- gruntfile.js
+++ gruntfile.js
@@ -20,7 +20,9 @@ module.exports = function(grunt){
       main: {
         files: [
           {expand: true, src: ['assets/**'], dest: 'build/<%= pkg.version %>/'},
-          {expand: true, src: ['./package.json', 'LICENSE.markdown', 'README.markdown'], dest: 'build/<%= pkg.version %>/', filter: 'isFile'}
+          {expand: true, src: ['./package.json', 'LICENSE.markdown', 'README.markdown'], dest: 'build/<%= pkg.version %>/', filter: 'isFile'},
+          {src: 'build/<%= pkg.version %>/lib/application.js', dest: 'build/<%= pkg.version %>/lib/index.js', filter: 'isFile'},
+          {src: 'build/<%= pkg.version %>/lib/client/client.js', dest: 'build/<%= pkg.version %>/lib/client/index.js', filter: 'isFile'}
         ]
       }
     }
